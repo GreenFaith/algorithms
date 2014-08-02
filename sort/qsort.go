b2cd72d7a995e29e5059a3f4295330edcbb713f2
oldlinux <yangcheng0000@qq.com>
2014-08-02 14:11:52 +0800

    add quick sort

---
+++ sort/qsort.go
@@ -0,0 +1,56 @@
+package algorithms
+//import "fmt"
+type Ord interface {
+    Swap(i, j int)  
+    Less(i,j int) bool 
+    Len() int
+} 
+
+type intArray []int
+type strArray []string
+
+//methods for intArray
+func (p intArray) Swap(i int, j int){
+    p[i], p[j] = p[j], p[i]
+}
+
+func (p intArray) Len() int{
+    return len(p)
+}
+
+func (p intArray) Less(i,j int) bool{
+    return p[i] < p[j]
+}
+
+//methods for strArray
+func (p strArray) Swap(i, j int){
+    p[i], p[j] = p[j], p[i]
+}
+func (p strArray) Len() int{
+    return len(p)
+}
+func (p strArray) Less(i, j int) bool{
+    return p[i] < p[j]
+}
+
+func Patition(A Ord, l int, r int) int {
+    var i int = l-1
+    for j:=l; j<r; j++ {
+        if A.Less(j, r) {
+            i = i + 1
+            A.Swap(j, i)
+        } 
+    }
+    A.Swap(r, i+1)
+    return i+1
+}
+
+func QuickSort(A Ord, l, r int) {
+    if l < r {
+        m := Patition(A, l, r)       
+        //fmt.Println(l, r, m)
+        QuickSort(A, l, m-1)
+        QuickSort(A, m+1, r)
+    }
+}
+
---
+++ sort/qsort_test.go
@@ -0,0 +1,53 @@
+package algorithms
+
+import (
+    "testing"
+    "math/rand"
+    //"fmt"
+)
+
+func TestPatition(t *testing.T) {
+    Max := 1000000;
+    var x = make(intArray, Max)
+
+    for i := 0; i < Max; i++ {
+        x[i] = rand.Intn(Max)
+    }
+
+    m := Patition(x, 0, Max-1)    
+
+    for i:=0; i<m; i++ {
+        if x[i] > x[m] {
+            t.FailNow()
+       }
+    }
+    for i:=m; i<Max; i++ {
+        if x[i] < x[m] {
+            t.FailNow()
+        }
+    }
+}
+
+func TestIntArray(t *testing.T){
+    Max := 1000000;
+    var x = make(intArray, Max)
+
+    for i := 0; i < Max; i++ {
+        x[i] = rand.Intn(Max)
+    }
+
+    QuickSort(x, 0, Max-1)
+
+    for i:=0; i<(Max-1); i++ {
+        if x[i+1] < x[i] {
+            t.Log("QuickSort Error") 
+            t.FailNow() 
+        }
+    }
+}
+
+func TestStrArray(t *testing.T){
+    x := strArray {"c","d","a","b"}
+    QuickSort(x,0, x.Len()-1)
+    t.Log(x)
+}
